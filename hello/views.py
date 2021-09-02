892689e19f5fd42077330bce2a6bf428f9ce781c
github username <taffanie.ben@gmail.com>
2021-09-02 17:23:09 +0100

    ✨ Added log message option with form

--- hello/views.py
+++ hello/views.py
@@ -28,6 +28,10 @@ import re
 from django.utils.timezone import datetime
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.shortcuts import redirect
+from hello.forms import LogMessageForm
+from hello.models import LogMessage
+from django.views.generic import ListView
 
 
 def hello_there(request, name):
@@ -41,8 +45,16 @@ def hello_there(request, name):
     )
 
 
-def home(request):
-    return render(request, "hello/home.html")
+# def home(request):
+#     return render(request, "hello/home.html")
+
+class HomeListView(ListView):
+    """Renders the home page, with a list of all messages."""
+    model = LogMessage
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeListView, self).get_context_data(**kwargs)
+        return context
 
 
 def about(request):
@@ -51,3 +63,16 @@ def about(request):
 
 def contact(request):
     return render(request, "hello/contact.html")
+
+
+def log_message(request):
+    form = LogMessageForm(request.POST or None)
+
+    if request.method == "POST":
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.log_date = datetime.now()
+            message.save()
+            return redirect("home")
+    else:
+        return render(request, "hello/log_message.html", {"form": form})
