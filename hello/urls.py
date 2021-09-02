05ae23bb787100105f285dca3b4b36bc2ce1f85c
github username <taffanie.ben@gmail.com>
2021-09-02 17:34:53 +0100

    ✨ Added superuser & admin page

--- hello/urls.py
+++ hello/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from hello import views
 from hello.models import LogMessage
+from django.contrib import admin
 
 home_list_view = views.HomeListView.as_view(
     # :5 limits the results to the five most recent
@@ -11,6 +12,8 @@ home_list_view = views.HomeListView.as_view(
 
 urlpatterns = [
     # path("", views.home, name="home"),
+    # This path is included by default when creating the app
+    path("admin/", admin.site.urls),
     path("", home_list_view, name="home"),
     path("hello/<name>", views.hello_there, name="hello_there"),
     path("about/", views.about, name="about"),
